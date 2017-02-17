cd2ef23ee71deab41290f580e9afbb866945f55b
tamasc <tamas.czerjak@gmail.com>
2017-02-17 10:12:09 +0100

    the right beugro is done finaly

--- beugro.js
+++ beugro.js
@@ -1,23 +1,22 @@
 // Create a function that takes an array of integers and returns the average of the even numbers from that array
 
-function average1(arr) {
-  return arr.reduce((a, c) => {return a + c}, 0) / arr.length;
-}
-
-function average2(arr) {
+function average(arr) {
   const n = arr.length;
+  let evenNums = 0;
   let sum = 0;
   for (let i = 0; i < n; i++) {
     if (typeof arr[i] !== 'number' || arr[i] % 1 !== 0) {
       console.error('array contains non integer elements');
       return;
     }
-    sum = sum + arr[i];
+    if (arr[i] % 2 === 0) {
+      sum = sum + arr[i];
+      evenNums++;
+    }
   }
-  return sum/n;
+  return sum/evenNums;
 }
 
-arr = [1, 2, 3, 1, 2, 3, 1, 1, 1, 2, 2, 2, 3, 3, 3];
+arr = [2, 2, 2, 2, 2, 2, 8];
 
-console.log(average1(arr));
-console.log(average2(arr));
+console.log(average(arr));
